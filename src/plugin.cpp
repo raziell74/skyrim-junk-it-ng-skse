9d505e9325ef2e6962903f05cfb9bf8c9fd211f4
Jordan Richmeier <jordan.richmeier2@gmail.com>
2026-08-27 07:37:47 -0600

    Plugin Version bump

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -16,7 +16,7 @@
 SKSE_EXPORT constinit SKSE::PluginVersionData SKSEPlugin_Version = []() noexcept {
 	SKSE::PluginVersionData v;
 	v.PluginName("JunkIt");
-	v.PluginVersion({ 0, 2, 0, 8 });
+	v.PluginVersion({ 0, 2, 0, 9 });
 	v.UsesAddressLibrary();
 	v.UsesUpdatedStructs();
 	return v;
